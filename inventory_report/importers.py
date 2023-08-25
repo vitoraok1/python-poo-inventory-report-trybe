a880fa2302f1e7bd9f6f474674e361091c387421
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-25 14:26:37 -0300

    requirement 03 - creating importer interface

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,18 @@
 from typing import Dict, Type
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
+
+""" abstract method importer reference:
+https://docs.python.org/pt-br/3.7/library/abc.html"""
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str):
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        ...
 
 
 class JsonImporter:
