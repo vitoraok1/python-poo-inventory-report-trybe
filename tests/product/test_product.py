034f1701824c0a93a562f0bedbc3c76fbe2e46ad
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-25 13:34:52 -0300

    requirement 01 - creating product tests

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "000",
+        "Product Name",
+        "Company Name",
+        "2023-08-25",
+        "2023-12-31",
+        "ABC123DEF456",
+        "Keep in a cool and dry place",
+    )
+
+    assert product.id == "000"
+    assert product.product_name == "Product Name"
+    assert product.company_name == "Company Name"
+    assert product.manufacturing_date == "2023-08-25"
+    assert product.expiration_date == "2023-12-31"
+    assert product.serial_number == "ABC123DEF456"
+    assert product.storage_instructions == "Keep in a cool and dry place"
