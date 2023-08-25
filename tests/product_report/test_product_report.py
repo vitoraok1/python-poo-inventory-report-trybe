cabebffa495cb6de707ac4bfa897cefacf2b3f85
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-25 14:14:19 -0300

    requirement 02 - creating product individual report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "000",
+        "Product Name",
+        "Company Name",
+        "2023-08-25",
+        "2023-12-31",
+        "ABC123DEF456",
+        "Keep in a cool and dry place",
+    )
+    expected_string = (
+        "The product 000 - Product Name with serial number ABC123DEF456 "
+        "manufactured on 2023-08-25 by the company Company Name "
+        "valid until 2023-12-31 must be stored according to the following "
+        "instructions: Keep in a cool and dry place."
+    )
+
+    actual_string = str(product)
+
+    assert actual_string == expected_string
