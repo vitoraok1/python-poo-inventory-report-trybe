e0b3ef10956cd739a7398c9fd1a4a664e640b175
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-25 14:40:04 -0300

    requirement 05 - creating Inventory class and methods

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self._data = data if data is not None else []
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
+
+    def add_data(self, data: list[Product]) -> None:
+        self._data.extend(data)
